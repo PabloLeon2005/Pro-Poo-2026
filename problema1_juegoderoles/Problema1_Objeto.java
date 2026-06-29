6f5560179d913614389a7cc7ffe29fd7a9f2ce7c
PabloLeon2005 <pabloguaman2005@gmail.com>
2026-06-29 17:03:36 -0500

    feat: interfaz base Problema1_Objeto implementando ISP

---
+++ problema1_juegoderoles/Problema1_Objeto.java
@@ -0,0 +1,7 @@
+package problema1_juegoderoles;
+
+// ISP (Interface Segregation Principle): Interfaz específica para objetos equipables, evitando métodos innecesarios.
+public interface Problema1_Objeto {
+    String getNombre();
+    void usar();
+}
