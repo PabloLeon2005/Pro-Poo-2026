22d020fd587dd777298307a33d8a382de1a521a6
PabloLeon2005 <pabloguaman2005@gmail.com>
2026-06-29 17:30:12 -0500

    feat: clase principal Problema1_JuegodeRoles (Parte 1)

---
+++ problema1_juegoderoles/Problema1_JuegodeRoles.java
@@ -0,0 +1,13 @@
+package problema1_juegoderoles;
+
+public class Problema1_JuegodeRoles {
+    public static void main(String[] args) {
+        Problema1_Personaje guerrero = new Problema1_Guerrero("Thor");
+        
+        Problema1_Arma espada = new Problema1_Arma("Espada Larga", 25);
+        Problema1_Armadura escudo = new Problema1_Armadura("Escudo de Hierro", 15);
+        
+        guerrero.getInventario().agregarObjeto(espada);
+        guerrero.getInventario().agregarObjeto(escudo);
+    }
+}
